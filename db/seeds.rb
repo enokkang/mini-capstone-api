c97cc9364ab151d634a3dcdd2ce05bfd4dc09bdc
enoch <enokkang@gmail.com>
2023-07-06 12:01:36 -0500

    adds seeds db and jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new({ name: "Sample1", price: 100, image_url: "https://avatars.githubusercontent.com/u/137811720?v=4", description: "This is a sample product." })
+product.save
