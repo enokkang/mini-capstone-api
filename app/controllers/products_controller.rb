41598d002c000a6554a00a0536e22869b0927c13
enoch <enokkang@gmail.com>
2023-07-07 14:00:31 -0500

    adds POST verb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,9 @@ class ProductsController < ApplicationController
     #render json: { product: @product }
     render template: "products/index"
   end
+
+  def create
+    @product = Product.create(name: "SamplePOST", price: 999, image_url: "about:blank", description: "POST Sample")
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get "/products", controller: "products", action: "index"
 
   get "/products/:id" => "products#show"
+
+  post "/products" => "products#create"
 end
