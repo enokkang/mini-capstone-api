455f5c7855ee0ebb2a908720a817729098246659
enoch <enokkang@gmail.com>
2023-07-11 15:03:27 -0500

    .

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,17 @@
 class Product < ApplicationRecord
   def is_discount?
-    is_discount = false
-    is_discount = true if price <= 10
-    is_discount
+    #is_discount = false
+    #is_discount = true if price <= 10
+    #is_discount
+
+    #alternative way:
+    price <= 10
   end
 
   def tax
     #price * 0.09
     (price * 0.09).round(3)
+    #"tax is : #{(price * 0.09).round(3)}"
   end
 
   def total
