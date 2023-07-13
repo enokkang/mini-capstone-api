5d043c2d649bb1bf0ff4d82fa471a7b000ddfa27
enoch <enokkang@gmail.com>
2023-07-13 14:19:54 -0500

    adds supplier_id and Supplier model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_185918) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_190544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_185918) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity" #, default: 0
+    t.integer "quantity", default: 0
+    t.integer "supplier_id"
   end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
