6a33e48b063a82a8ab7a86cc383649882a8781bc
enoch <enokkang@gmail.com>
2023-08-25 13:17:17 -0500

    deleted migration files

--- db/migrate/20230721204959_rename_image_id_from_products.rb
+++
@@ -1,5 +0,0 @@
-class RenameImageIdFromProducts < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :products, :image_id, :images
-  end
-end
--- db/migrate/20230724200740_rename.rb
+++
@@ -1,5 +0,0 @@
-class Rename < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :carted_products, :quantity_integer, :quantity
-  end
-end
